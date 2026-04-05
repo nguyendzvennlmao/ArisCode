d6f50bb9175f3db9f21adeb6818c64b55a5b5212
nguyendzvennlmao <tqnmods111@gmail.com>
2026-04-05 10:04:40 +0700

    Update ItemBuilder.java

--- src/main/java/me/aris/ariscode/utils/ItemBuilder.java
+++ src/main/java/me/aris/ariscode/utils/ItemBuilder.java
@@ -1,6 +1,5 @@
 package me.aris.ariscode.utils;
 
-import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -20,44 +19,50 @@ public class ItemBuilder {
     public static ItemStack create(Material material, String name) {
         ItemStack item = new ItemStack(material);
         ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(MessageUtils.color(name));
-        item.setItemMeta(meta);
+        if (meta != null) {
+            meta.setDisplayName(MessageUtils.color(name));
+            item.setItemMeta(meta);
+        }
         return item;
     }
     
     public static ItemStack create(Material material, String name, List<String> lore) {
         ItemStack item = new ItemStack(material);
         ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(MessageUtils.color(name));
-        
-        List<String> coloredLore = new ArrayList<>();
-        for (String line : lore) {
-            coloredLore.add(MessageUtils.color(line));
+        if (meta != null) {
+            meta.setDisplayName(MessageUtils.color(name));
+            List<String> coloredLore = new ArrayList<>();
+            for (String line : lore) {
+                coloredLore.add(MessageUtils.color(line));
+            }
+            meta.setLore(coloredLore);
+            item.setItemMeta(meta);
         }
-        meta.setLore(coloredLore);
-        item.setItemMeta(meta);
         return item;
     }
     
-    public static ItemStack create(Material material, int amount, int data, String name) {
+    public static ItemStack create(Material material, int amount, String name) {
         ItemStack item = new ItemStack(material, amount);
         ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(MessageUtils.color(name));
-        item.setItemMeta(meta);
+        if (meta != null) {
+            meta.setDisplayName(MessageUtils.color(name));
+            item.setItemMeta(meta);
+        }
         return item;
     }
     
-    public static ItemStack create(Material material, int amount, int data, String name, List<String> lore) {
+    public static ItemStack create(Material material, int amount, String name, List<String> lore) {
         ItemStack item = new ItemStack(material, amount);
         ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(MessageUtils.color(name));
-        
-        List<String> coloredLore = new ArrayList<>();
-        for (String line : lore) {
-            coloredLore.add(MessageUtils.color(line));
+        if (meta != null) {
+            meta.setDisplayName(MessageUtils.color(name));
+            List<String> coloredLore = new ArrayList<>();
+            for (String line : lore) {
+                coloredLore.add(MessageUtils.color(line));
+            }
+            meta.setLore(coloredLore);
+            item.setItemMeta(meta);
         }
-        meta.setLore(coloredLore);
-        item.setItemMeta(meta);
         return item;
     }
 }
